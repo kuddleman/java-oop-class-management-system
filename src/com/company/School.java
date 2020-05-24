0e61e1538b5579bc8baef2543f79919e2696c5cd
Kuddleman <edl1155@gmail.com>
2020-05-24 13:42:10 -0700

    add school class

--- src/com/company/School.java
+++ src/com/company/School.java
@@ -1,4 +1,53 @@
 package com.company;
 
+/**
+ * Many teachers, many students
+ * Implement teachers and students using an arraylist
+ */
+
 public class School {
+
+    private List<Teacher> teachers;
+    private List<Student> students;
+    private int totalMoneyEarned;
+    private int totalMoneySpent;
+
+    public School(List<Teacher> teachers, List<Student> students) {
+        this.teachers = teachers;
+        this.students = students;
+        totalMoneyEarned = 0;
+        totalMoneySpent = 0;
+    }
+
+    public List<Teacher> getTeachers() {
+        return teachers;
+    }
+
+    public void addTeachers(Teacher teacher) {
+        teachers.add(teacher);
+    }
+
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public void addStudent(Student student) {
+        students.add(student);
+    }
+
+    public int updateTotalMoneyEarned( int MoneyEarned) {
+         totalMoneyEarned += MoneyEarned;
+    }
+
+    public void setTotalMoneyEarned(int totalMoneyEarned) {
+        this.totalMoneyEarned = totalMoneyEarned;
+    }
+
+    public int getTotalMoneySpent() {
+        return totalMoneySpent;
+    }
+
+    public void updateTotalMoneySpent(int moneySpent) {
+        totalMoneyEarned -= MoneySpent;
+    }
 }
