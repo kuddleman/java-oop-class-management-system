630324ed5d28918b4d84ef5954a5c3d192fe20d1
Kuddleman <edl1155@gmail.com>
2020-05-24 12:56:25 -0700

    add setGrade

--- src/com/company/Student.java
+++ src/com/company/Student.java
@@ -14,6 +14,21 @@ public class Student {
 
     public Student( int id, String name, int grade) {
 
+      this.feesPaid = 0;
+      this.feesTotal = 30000;
+      this.id = id;
+      this.name = name;
+      this.grade = grade;
+    }
+
+    //not going to alter student's name, id
+    // no need for setter
+
+    //we will alter: student's grade
+
+
+    public void setGrade(int grade) {
+        this.grade = grade;
     }
 }
 
