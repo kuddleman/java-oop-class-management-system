6e609d4266b5f49781c02f22d3dad2caf20d4000
Kuddleman <edl1155@gmail.com>
2020-05-24 12:47:17 -0700

    initial commit

---
+++ src/com/company/Student.java
@@ -0,0 +1,20 @@
+package com.company;
+
+//this class is responsible
+// for keeping track of students'
+// fees, name, grade and fees paid
+
+public class Student {
+
+    private int id;
+    private String name;
+    private int grade;
+    private int feesPaid;
+    private int feesTotal;
+
+    public Student( int id, String name, int grade) {
+
+    }
+}
+
+
